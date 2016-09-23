82b871eab7d9736b8c33fd545a61a1ea57476151
Keith Cirkel <keithamus@users.noreply.github.com>
2016-09-23 16:47:55 +0100

    fix: change o- prefixes to n-

--- src/js/sliding-popup.js
+++ src/js/sliding-popup.js
@@ -2,24 +2,24 @@ class SlidingPopup {
 
 	constructor (el) {
 		if (!el) {
-			el = document.querySelector('[data-o-component="o-sliding-popup"]');
+			el = document.querySelector('[data-n-component="n-sliding-popup"]');
 		} else if (typeof el === 'string') {
 			el = document.querySelector(el);
 		}
 		this.el = el;
-		el.classList.add('o-sliding-popup--active');
-		const close = el.querySelector('[data-o-component="o-sliding-popup-close"]');
+		el.classList.add('n-sliding-popup--active');
+		const close = el.querySelector('[data-n-component="n-sliding-popup-close"]');
 		if (close) {
 			close.addEventListener('click', () => this.close());
 		}
 	}
 
 	open () {
-		this.el.setAttribute('data-o-sliding-popup-visible', 'true');
+		this.el.setAttribute('data-n-sliding-popup-visible', 'true');
 	}
 
 	close () {
-		this.el.removeAttribute('data-o-sliding-popup-visible');
+		this.el.removeAttribute('data-n-sliding-popup-visible');
 		const event = new CustomEvent('close', { detail: { target: this.el, instance: this }});
 		if (this.el.onClose) {
 			this.el.onClose.call(null, event);
