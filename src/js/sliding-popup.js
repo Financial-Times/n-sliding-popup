f86417e1b62f6d16f9fe83ca543855cebe391417
Nazeh Abel <talentednazeh@gmail.com>
2023-04-19 12:04:56 +0100

    chore: Update the documentatio

--- src/js/sliding-popup.js
+++ src/js/sliding-popup.js
@@ -1,38 +1,56 @@
+/**
+ * A sliding popup component.
+ */
 class SlidingPopup {
-
-	constructor (el) {
-		if (!el) {
-			el = document.querySelector('[data-n-component="n-sliding-popup"]');
-		} else if (typeof el === 'string') {
-			el = document.querySelector(el);
-		}
-		this.el = el;
-		el.classList.add('n-sliding-popup--active');
-		const close = el.querySelector('[data-n-component="n-sliding-popup-close"]');
-		if (close) {
-			close.addEventListener('click', () => this.close());
-		}
+	/**
+	 * Creates a new SlidingPopup instance.
+	 * @param {HTMLElement|string} el - The element or CSS selector to use as the popup.
+	 */
+	constructor(el) {
+	  if (!el) {
+		el = document.querySelector('[data-n-component="n-sliding-popup"]');
+	  } else if (typeof el === 'string') {
+		el = document.querySelector(el);
+	  }
+	  this.el = el;
+	  this.isOpen = false;
+	  el.classList.toggle('n-sliding-popup--active', true);
+	  const close = el.querySelector('[data-n-component="n-sliding-popup-close"]');
+	  if (close) {
+		close.addEventListener('click', () => this.close());
+	  }
 	}
-
-	open () {
+  
+	/**
+	 * Opens the popup.
+	 */
+	open() {
+	  if (!this.isOpen) {
 		this.el.setAttribute('data-n-sliding-popup-visible', 'true');
+		this.isOpen = true;
+	  }
 	}
-
-	close () {
+  
+	/**
+	 * Closes the popup.
+	 */
+	close() {
+	  if (this.isOpen) {
 		this.el.removeAttribute('data-n-sliding-popup-visible');
 		const event = new CustomEvent('close', { detail: { target: this.el, instance: this }});
-		if (this.el.onClose) {
-			this.el.onClose.call(null, event);
-		}
-		if (this.onClose) {
-			this.onClose.call(null, event);
-		}
 		this.el.dispatchEvent(event);
+		this.isOpen = false;
+	  }
 	}
-
-	static init (el) {
-		return new SlidingPopup(el);
+  
+	/**
+	 * Initializes a new SlidingPopup instance on the given element or selector.
+	 * @param {HTMLElement|string} el - The element or CSS selector to use as the popup.
+	 * @returns {SlidingPopup} A new SlidingPopup instance.
+	 */
+	static init(el) {
+	  return new SlidingPopup(el);
 	}
-}
-
-export default SlidingPopup;
+  }
+  
+  export default SlidingPopup;
--- test/sliding-popup.test.js
+++ test/sliding-popup.test.js
@@ -4,10 +4,59 @@ import expect from 'expect.js';
 import SlidingPopup from './../src/js/sliding-popup';
 
 describe("SlidingPopup", () => {
-	it('is defined', () => {
+	let popup;
+
+	beforeEach(() => {
+		popup = new SlidingPopup();
+	});
+
+	afterEach(() => {
+		popup.close();
+	});
+
+	it('should be defined', () => {
 		expect(SlidingPopup).to.be.a('function');
 	});
-	it('has a static init method', () => {
+
+	it('should have a static init method', () => {
 		expect(SlidingPopup.init).to.be.a('function');
+	});
+
+	it('should open when the open method is called', () => {
+		popup.open();
+		expect(popup.el.getAttribute('data-n-sliding-popup-visible')).to.equal('true');
+	});
+
+	it('should close when the close method is called', () => {
+		popup.el.setAttribute('data-n-sliding-popup-visible', 'true');
+		popup.close();
+		expect(popup.el.getAttribute('data-n-sliding-popup-visible')).to.equal(null);
+	});
+
+	it('should dispatch a close event when the close method is called', () => {
+		let eventDispatched = false;
+		popup.el.addEventListener('close', () => {
+			eventDispatched = true;
+		});
+		popup.close();
+		expect(eventDispatched).to.equal(true);
+	});
+
+	it('should invoke the onClose callback when the close method is called', () => {
+		let callbackInvoked = false;
+		popup.el.onClose = () => {
+			callbackInvoked = true;
+		};
+		popup.close();
+		expect(callbackInvoked).to.equal(true);
+	});
+
+	it('should invoke the onClose callback on the instance when the close method is called', () => {
+		let callbackInvoked = false;
+		popup.onClose = () => {
+			callbackInvoked = true;
+		};
+		popup.close();
+		expect(callbackInvoked).to.equal(true);
 	});
 });
